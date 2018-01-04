b540b57f93b111748a65558cede2d93b464ac3dd
pelloKraken628 <lightfury.gpsh@gmail.com>
2018-01-04 20:22:15 +0100

    Helpers package

---
+++ helpers/__init__.py
@@ -0,0 +1,14 @@
+"""
+  @author Victor I. Afolabi
+  A.I. engineer/researcher & Software engineer
+  javafolabi@gmail.com
+  
+  Created on 04 January, 2018 @ 8:21 PM.
+  
+  Copyright © 2018. Victor. All rights reserved.
+"""
+import helpers.config
+
+__all__ = [
+    'config',
+]
