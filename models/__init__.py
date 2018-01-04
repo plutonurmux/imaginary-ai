980820661248c0cc3fa1bbe3da9680ad7f40c85a
pelloKraken628 <lightfury.gpsh@gmail.com>
2018-01-04 17:58:24 +0100

    Model package

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+"""
+  @author Victor I. Afolabi
+  A.I. engineer/researcher & Software engineer
+  javafolabi@gmail.com
+  
+  Created on 04 January, 2018 @ 5:57 PM.
+  
+  Copyright © 2018. Victor. All rights reserved.
+"""
+__all__ = [
+
+]
