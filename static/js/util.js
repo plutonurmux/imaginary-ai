1731d8e6bbc2021d502e3dee0044cda7479cb5ac
pelloKraken628 <lightfury.gpsh@gmail.com>
2018-01-08 17:12:59 +0100

    Image preview demo [Not working]

--- static/js/util.js
+++ static/js/util.js
@@ -20,8 +20,8 @@
       b.push(
         '<a ' +
         'class="link depth-' + indent + '"' +
-        ((typeof target !== 'undefined' && target != '') ? ' target="' + target + '"' : '') +
-        ((typeof href !== 'undefined' && href != '') ? ' href="' + href + '"' : '') +
+        ((typeof target !== == 'undefined' && target !== '') ? ' target="' + target + '"' : '') +
+        ((typeof href !== == 'undefined' && href !== '') ? ' href="' + href + '"' : '') +
         '>' +
         '<span class="indent-' + indent + '"></span>' +
         $this.text() +
@@ -42,7 +42,7 @@
   $.fn.panel = function (userConfig) {
 
     // No elements?
-    if (this.length == 0)
+    if (this.length === 0)
       return $this;
 
     // Multiple elements?
@@ -95,7 +95,7 @@
     }, userConfig);
 
     // Expand "target" if it's not a jQuery object already.
-    if (typeof config.target != 'jQuery')
+    if (typeof config.target !== 'jQuery')
       config.target = $(config.target);
 
     // Panel.
@@ -153,7 +153,7 @@
             href = $a.attr('href'),
             target = $a.attr('target');
 
-          if (!href || href == '#' || href == '' || href == '#' + id)
+          if (!href || href === '#' || href === '' || href === '#' + id)
             return;
 
           // Cancel original event.
@@ -166,7 +166,7 @@
           // Redirect to href.
           window.setTimeout(function () {
 
-            if (target == '_blank')
+            if (target === '_blank')
               window.open(href);
             else
               window.location.href = href;
@@ -287,7 +287,7 @@
     if (config.hideOnEscape)
       $window.on('keydown', function (event) {
 
-        if (event.keyCode == 27)
+        if (event.keyCode === 27)
           $this._hide(event);
 
       });
@@ -303,11 +303,11 @@
   $.fn.placeholder = function () {
 
     // Browser natively supports placeholders? Bail.
-    if (typeof (document.createElement('input')).placeholder != 'undefined')
+    if (typeof (document.createElement('input')).placeholder !== 'undefined')
       return $(this);
 
     // No elements?
-    if (this.length == 0)
+    if (this.length === 0)
       return $this;
 
     // Multiple elements?
@@ -329,8 +329,8 @@
 
         var i = $(this);
 
-        if (i.val() == ''
-          || i.val() == i.attr('placeholder'))
+        if (i.val() === ''
+          || i.val() === i.attr('placeholder'))
           i
             .addClass('polyfill-placeholder')
             .val(i.attr('placeholder'));
@@ -343,7 +343,7 @@
         if (i.attr('name').match(/-polyfill-field$/))
           return;
 
-        if (i.val() == '')
+        if (i.val() === '')
           i
             .addClass('polyfill-placeholder')
             .val(i.attr('placeholder'));
@@ -356,7 +356,7 @@
         if (i.attr('name').match(/-polyfill-field$/))
           return;
 
-        if (i.val() == i.attr('placeholder'))
+        if (i.val() === i.attr('placeholder'))
           i
             .removeClass('polyfill-placeholder')
             .val('');
@@ -377,16 +377,16 @@
             .replace(/type=password/i, 'type=text')
         );
 
-        if (i.attr('id') != '')
+        if (i.attr('id') !== '')
           x.attr('id', i.attr('id') + '-polyfill-field');
 
-        if (i.attr('name') != '')
+        if (i.attr('name') !== '')
           x.attr('name', i.attr('name') + '-polyfill-field');
 
         x.addClass('polyfill-placeholder')
           .val(x.attr('placeholder')).insertAfter(i);
 
-        if (i.val() == '')
+        if (i.val() === '')
           i.hide();
         else
           x.hide();
@@ -398,7 +398,7 @@
 
             var x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-            if (i.val() == '') {
+            if (i.val() === '') {
 
               i.hide();
               x.show();
@@ -442,7 +442,7 @@
             if (i.attr('name').match(/-polyfill-field$/))
               i.attr('name', '');
 
-            if (i.val() == i.attr('placeholder')) {
+            if (i.val() === i.attr('placeholder')) {
 
               i.removeClass('polyfill-placeholder');
               i.val('');
@@ -478,7 +478,7 @@
 
                 x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
 
-                if (i.val() == '') {
+                if (i.val() === '') {
                   i.hide();
                   x.show();
                 }
@@ -498,7 +498,7 @@
               case 'textarea':
                 i.val(i.attr('defaultValue'));
 
-                if (i.val() == '') {
+                if (i.val() === '') {
                   i.addClass('polyfill-placeholder');
                   i.val(i.attr('placeholder'));
                 }
@@ -528,7 +528,7 @@
     var key = '__prioritize';
 
     // Expand $elements if it's not already a jQuery object.
-    if (typeof $elements != 'jQuery')
+    if (typeof $elements !== 'jQuery')
       $elements = $($elements);
 
     // Step through elements.
@@ -538,7 +538,7 @@
         $parent = $e.parent();
 
       // No parent? Bail.
-      if ($parent.length == 0)
+      if ($parent.length === 0)
         return;
 
       // Not moved? Move it.
@@ -552,7 +552,7 @@
         $p = $e.prev();
 
         // Couldn't find anything? Means this element's already at the top, so bail.
-        if ($p.length == 0)
+        if ($p.length === 0)
           return;
 
         // Move element to top of parent.
@@ -584,6 +584,11 @@
 
   };
 
+  /**
+   * +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+
+   * | Uploading from file
+   * +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+
+   */
   $('#upload-file').on('change', function (e) {
     console.log($(this)[0].files);
     var file = $(this)[0].files;
@@ -593,6 +598,11 @@
     }
   });
 
+  /**
+   * +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+
+   * | Camera capture upload
+   * +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+
+   */
   $('#camera-file').on('change', function (e) {
     var file = $(this)[0].files;
     if (file.length === 1) {
@@ -600,5 +610,31 @@
       $('#image-url').attr('placeholder', filename);
     }
   });
+
+  /**
+   * +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+
+   * | Preview uploads
+   * +–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+
+   */
+  function show_preview(file_id) {
+    var preview_image = $('#preview-image');
+    new AjaxUpload(file_id, {
+      action: $('image-search-form').attr('action'),
+      name: 'image',
+      onSubmit: function (file, extension) {
+        $('#preview').addClass('loading');
+      },
+      onComplete: function (file, response) {
+        preview_image.load(function () {
+          $('#preview').removeClass('loading');
+          preview_image.unbind();
+        });
+        preview_image.attr('src', response)
+      }
+    })
+  }
+
+  show_preview('upload-file');
+  show_preview('camera-file');
 
 })(jQuery);
