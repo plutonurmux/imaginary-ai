ccc6a6825e1424c73643f2fbc53ea9ae4e2c5b66
pelloKraken628 <lightfury.gpsh@gmail.com>
2018-01-24 22:03:19 +0100

    Update views/research.py

--- views/research.py
+++ views/research.py
@@ -15,10 +15,30 @@ from views import app, back
 @app.route('/research/')
 @back.anchor
 def research():
+    """Research page.
+
+    Decorators:
+        back
+        app
+
+    Returns:
+        [render_template] -- [/research/ html template.]
+    """
+
     return render_template('research/index.html')
 
 
 @app.route('/research/a-language-model/')
 @back.anchor
 def a_language_model():
+    """A language model research page.
+
+    Decorators:
+        back
+        app
+
+    Returns:
+        [render_template] -- [/research/a-langauge-model.html template.]
+    """
+
     return render_template('research/a-language-model.html')
