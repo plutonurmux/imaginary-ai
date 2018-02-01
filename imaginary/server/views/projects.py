13c8fd39112c4dcbc47c8c5d59260963e7c976a8
pelloKraken628 <lightfury.gpsh@gmail.com>
2018-02-02 00:05:32 +0100

    Configuration in progress

---
+++ imaginary/server/views/projects.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 01 February, 2018 @ 11:43 PM.
+  Copyright © 2018. Victor. All rights reserved.
+"""
