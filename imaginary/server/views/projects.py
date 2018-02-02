02e262e835fd17a66ba74c8991bbb65762953c4b
pelloKraken628 <lightfury.gpsh@gmail.com>
2018-02-02 16:44:08 +0100

    Project routes

--- imaginary/server/views/projects.py
+++ imaginary/server/views/projects.py
@@ -16,3 +16,33 @@ from imaginary.server import app, back
 @back.anchor
 def projects():
     return render_template('projects/index.html')
+
+
+@app.route('/projects/image-classification/')
+def image_classification():
+    return render_template('projects/image-classification.html')
+
+
+@app.route('/projects/generative-models/')
+def generative_models():
+    return render_template('projects/generative-models.html')
+
+
+@app.route('/projects/image-search/')
+def image_search():
+    return render_template('projects/image-search.html')
+
+
+@app.route('/projects/ai-articles/')
+def ai_articles():
+    return render_template('projects/ai-articles.html')
+
+
+@app.route('/projects/auto-encoding/')
+def auto_encoding():
+    return render_template('projects/auto-encoding.html')
+
+
+@app.route('/projects/reinforcement-learning/')
+def reinforcement_learning():
+    return render_template('projects/reinforcement-learning.html')
